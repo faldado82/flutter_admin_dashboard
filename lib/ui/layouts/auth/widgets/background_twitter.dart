3c54116df605d424871f91e9e7bed998cb70c39e
faldado82 <faldado82@gmail.com>
2023-07-05 15:04:35 -0300

    background_twitter 2

--- lib/ui/layouts/auth/widgets/background_twitter.dart
+++ lib/ui/layouts/auth/widgets/background_twitter.dart
@@ -6,13 +6,26 @@ class BackgroundTwitter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('twitter-bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('twitter-bg.png'),
-          fit: BoxFit.cover,
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: const Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Image(
+                image: AssetImage('twitter-white-logo.png'),
+                width: 400,
+              ),
+            ),
+          ),
         ),
       ),
-    ));
+    );
   }
 }
